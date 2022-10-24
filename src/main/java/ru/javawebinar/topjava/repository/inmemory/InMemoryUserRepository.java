77e3860f443cb7caf0917998603011d7fabc7128
Shahman Magomadov <magomadovshahman@gmail.com>
2022-10-24 19:55:15 +0300

    chagne UserRepo save

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -5,53 +5,56 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
 
-    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+	private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+	private final AtomicInteger counter = new AtomicInteger(0);
+	private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
-    private final AtomicInteger counter = new AtomicInteger(0);
-    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+	@Override
+	public boolean delete(int id) {
+		log.info("delete {}", id);
+		return repository.remove(id) != null;
+	}
 
-    @Override
-    public boolean delete(int id) {
-        log.info("delete {}", id);
-        return repository.remove(id) != null;
-    }
+	@Override
+	public User save(User user) {
+		log.info("save {}", user);
+		if (user.isNew()) {
+			user.setId(counter.incrementAndGet());
+			repository.put(user.getId(), user);
+			return user;
+		}
+		return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
+	}
 
-    @Override
-    public User save(User user) {
-        log.info("save {}", user);
-        if (user.isNew()) {
-            user.setId(counter.incrementAndGet());
-            repository.put(user.getId(), user);
-            return user;
-        }
-        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
-    }
+	@Override
+	public User get(int id) {
+		log.info("get {}", id);
+		return repository.get(id);
+	}
 
-    @Override
-    public User get(int id) {
-        log.info("get {}", id);
-        return repository.get(id);
-    }
+	@Override
+	public List<User> getAll() {
+		log.info("getAll");
+		return repository.values().stream()
+				.sorted((u1, u2) -> u1.getName().compareTo(u2.getName()))
+				.collect(Collectors.toList());
+	}
 
-    @Override
-    public List<User> getAll() {
-        log.info("getAll");
-        return (List<User>) repository.values();
-    }
-
-    @Override
-    public User getByEmail(String email) {
-        log.info("getByEmail {}", email);
-        return repository.get(getAll().stream().filter(m -> m.getEmail().equals(email)));
-    }
+	@Override
+	public User getByEmail(String email) {
+		log.info("getByEmail {}", email);
+		return repository.get(getAll().stream().filter(u -> u.getEmail().equals(email)));
+	}
 }
