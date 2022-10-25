2ce00567ccbfe345f2e4c636321fcde824a8f648
Shahman Magomadov <magomadovshahman@gmail.com>
2022-10-25 21:10:18 +0300

    add mealRestController in main

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -3,42 +3,38 @@ package ru.javawebinar.topjava.service;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.exception.NotFoundException;
-import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class MealService {
 
-    private final MealRepository repository;
+	private final MealRepository repository;
 
-    public MealService(MealRepository repository) {
-        this.repository = repository;
-    }
+	public MealService(MealRepository repository) {
+		this.repository = repository;
+	}
 
-    public Meal create(Meal meal) {
-        return repository.save(meal);
-    }
+	public Meal create(Meal meal) {
+		return repository.save(meal);
+	}
 
-    public void delete(int id) {
-        checkNotFoundWithId(repository.delete(id), id);
-    }
+	public void delete(int id) {
+		checkNotFoundWithId(repository.delete(id), id);
+	}
 
-    public Meal get(int id) {
-        return checkNotFoundWithId(repository.get(id), id);
-    }
+	public Meal get(int id) {
+		return checkNotFoundWithId(repository.get(id), id);
+	}
 
-    public Collection<Meal> getAll() {
-        return repository.getAll();
-    }
+	public Collection<Meal> getAll() {
+		return repository.getAll();
+	}
 
-    public void update(Meal meal) {
-        checkNotFoundWithId(repository.save(meal), meal.getId());
-    }
+	public void update(Meal meal) {
+		checkNotFoundWithId(repository.save(meal), meal.getId());
+	}
 
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -3,21 +3,25 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import ru.javawebinar.topjava.model.Meal;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
 import java.util.Collection;
-import java.util.List;
 
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
+@Controller
 public class MealRestController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
+    private final MealService service;
+
     @Autowired
-    private MealService service;
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
 
     public Collection<Meal> getAll() {
         log.info("getAll");
