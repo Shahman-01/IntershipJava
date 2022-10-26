299479f67bff3a43d597b1377a051ff30021576b
Shahman Magomadov <magomadovshahman@gmail.com>
2022-10-26 16:05:25 +0300

    fix MealService

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.service;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
 
@@ -18,23 +19,23 @@ public class MealService {
 	}
 
 	public Meal create(Meal meal) {
-		return repository.save(meal);
+		return repository.save(meal, SecurityUtil.authUserId());
 	}
 
 	public void delete(int id) {
-		checkNotFoundWithId(repository.delete(id), id);
+		checkNotFoundWithId(repository.delete(id, SecurityUtil.authUserId()), id);
 	}
 
 	public Meal get(int id) {
-		return checkNotFoundWithId(repository.get(id), id);
+		return checkNotFoundWithId(repository.get(id, SecurityUtil.authUserId()), id);
 	}
 
 	public Collection<Meal> getAll() {
-		return repository.getAll();
+		return repository.getAll(SecurityUtil.authUserId());
 	}
 
 	public void update(Meal meal) {
-		checkNotFoundWithId(repository.save(meal), meal.getId());
+		checkNotFoundWithId(repository.save(meal, SecurityUtil.authUserId()), meal.getId());
 	}
 
 }
