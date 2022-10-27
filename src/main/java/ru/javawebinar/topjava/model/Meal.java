2b7a877a9c514a2669ad8a147987a14e30964fb5
Shahman Magomadov <magomadovshahman@gmail.com>
2022-10-27 20:18:28 +0300

    did JdbcMealRepo, but not all yet

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,50 +5,65 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity {
-    private final LocalDateTime dateTime;
+	private LocalDateTime dateTime;
 
-    private final String description;
+	private String description;
 
-    private final int calories;
+	private int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
-    }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        super(id);
-        this.dateTime = dateTime;
-        this.description = description;
-        this.calories = calories;
-    }
+	public Meal() {
+	}
+	public Meal(LocalDateTime dateTime, String description, int calories) {
+		this(null, dateTime, description, calories);
+	}
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
+	public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+		super(id);
+		this.dateTime = dateTime;
+		this.description = description;
+		this.calories = calories;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
 
-    public int getCalories() {
-        return calories;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public LocalDate getDate() {
-        return dateTime.toLocalDate();
-    }
+	public int getCalories() {
+		return calories;
+	}
 
-    public LocalTime getTime() {
-        return dateTime.toLocalTime();
-    }
+	public LocalDate getDate() {
+		return dateTime.toLocalDate();
+	}
 
-    @Override
-    public String toString() {
-        return "Meal{" +
-                "id=" + id +
-                ", dateTime=" + dateTime +
-                ", description='" + description + '\'' +
-                ", calories=" + calories +
-                '}';
-    }
+	public LocalTime getTime() {
+		return dateTime.toLocalTime();
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+
+	@Override
+	public String toString() {
+		return "Meal{" +
+				"id=" + id +
+				", dateTime=" + dateTime +
+				", description='" + description + '\'' +
+				", calories=" + calories +
+				'}';
+	}
 }
