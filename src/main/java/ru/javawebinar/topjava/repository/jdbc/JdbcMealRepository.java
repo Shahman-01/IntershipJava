12239b46e00c441c938f8c4d9aced2fbdc60825d
Shahman Magomadov <magomadovshahman@gmail.com>
2022-10-31 18:04:55 +0300

    mod JdbcUserRepo

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.repository.jdbc;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -5,15 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
@@ -37,5 +40,32 @@ public class MealServiceTest {
 	public void delete() {
 		service.delete(MEAL1_ID, USER_ID);
 		assertThrows(NotFoundException.class, () -> service.get(MEAL1_ID, USER_ID));
+	}
+
+	@Test
+	public void deleteNotFound() {
+		assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND, USER_ID));
+	}
+
+	@Test
+	public void deleteNotOwn() {
+		assertThrows(NotFoundException.class, () -> service.delete(MEAL1_ID, ADMIN_ID));
+	}
+
+	@Test
+	public void duplicateDateTimeCreate() {
+		assertThrows(DataAccessException.class, () ->
+				service.create(new Meal(null, meal1.getDateTime(), "duplicate", 100), USER_ID));
+	}
+
+	@Test
+	public void get() {
+		Meal actual = service.get(ADMIN_MEAL_ID, ADMIN_ID);
+		assertMatch(actual, adminMeal1);
+	}
+
+	@Test
+	public void getAll() {
+		assertMatch(service.getAll(USER_ID), meals);
 	}
 }
