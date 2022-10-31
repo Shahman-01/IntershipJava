0c90e9d5b7e6bb12695a8ed491c050babdceb417
Shahman Magomadov <magomadovshahman@gmail.com>
2022-10-31 16:46:31 +0300

    add MealTestData, not finished

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+
+import static ru.javawebinar.topjava.MealTestDate.*;
 
 @ContextConfiguration({
 		"classpath:spring/spring-app.xml",
@@ -23,5 +27,13 @@ public class MealServiceTest {
 	@Autowired
 	private MealService service;
 
-
+	@Test
+	public void create() {
+		Meal created = service.create(getNew(), USER_ID);
+		Integer newId = created.getId();
+		Meal newMeal = getNew();
+		newMeal.setId(newId);
+		assertMatch(created, newMeal);
+		assertMatch(service.get(newId, USER_ID), newMeal);
+	}
 }
