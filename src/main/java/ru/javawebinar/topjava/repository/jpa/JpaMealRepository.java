5a303d0f23b75ac5a56e0f0847badd7ee6179e7c
Shahman Magomadov <magomadovshahman@gmail.com>
2022-11-02 00:53:38 +0300

    mod getBetweenHalfOpen

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -5,13 +5,15 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
 
 	@PersistenceContext
@@ -40,9 +42,13 @@ public class JpaMealRepository implements MealRepository {
 
 	@Override
 	public Meal get(int id, int userId) {
-//		return em.find(Meal.class, id);
-		return (Meal) em.createQuery("SELECT m FROM Meal m WHERE m.id = " + id + " AND m.user.id =" + userId)
-				.getResultList().get(0);
+		Meal meal = em.find(Meal.class, id);
+		if (meal != null &&
+				meal.getUser().getId().equals(userId))
+			return meal;
+		throw new NotFoundException("not found meal whit id = " + id);
+//		return (Meal) em.createQuery("SELECT m FROM Meal m WHERE m.id = " + id + " AND m.user.id =" + userId)
+//				.getResultList().get(0);
 	}
 
 	@Override
@@ -53,6 +59,16 @@ public class JpaMealRepository implements MealRepository {
 
 	@Override
 	public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-		return getAll(userId);
+		List<Meal> meals = em.createQuery("SELECT m FROM Meal m WHERE m.user.id = " + userId + " ORDER BY m.dateTime DESC")
+				.getResultList();
+
+		List<Meal> finalList = new ArrayList<>();
+		for (Meal m : meals) {
+			if (m.getDateTime().isAfter(startDateTime) &&
+					m.getDateTime().isBefore(endDateTime))
+				finalList.add(m);
+		}
+
+		return finalList;
 	}
 }
