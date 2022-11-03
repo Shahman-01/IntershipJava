4d709e1e1d180010348972822291d81ffca5b492
Shahman Magomadov <magomadovshahman@gmail.com>
2022-11-03 21:28:21 +0300

    mod JpaMealRepo

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -22,53 +22,44 @@ public class JpaMealRepository implements MealRepository {
 	@Override
 	@Transactional
 	public Meal save(Meal meal, int userId) {
-		User user = em.find(User.class, userId);
-		meal.setUser(user);
+		meal.setUser(em.getReference(User.class, userId));
 		if (meal.isNew()) {
 			em.persist(meal);
 			return meal;
-		} else {
-			return em.merge(meal);
+		} else if (get(meal.id(), userId) == null) {
+			return null;
 		}
+		return em.merge(meal);
 	}
 
 	@Override
 	@Transactional
 	public boolean delete(int id, int userId) {
-		Query query = em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user.id = " + userId);
-		return query.setParameter("id", id)
+		return em.createNamedQuery(Meal.DELETE)
+				.setParameter("id", id)
+				.setParameter("userId", userId)
 				.executeUpdate() != 0;
 	}
 
 	@Override
 	public Meal get(int id, int userId) {
 		Meal meal = em.find(Meal.class, id);
-		if (meal != null &&
-				meal.getUser().getId().equals(userId)) {
-			meal.setUser(null);
-			return meal;
-		}
-		throw new NotFoundException("not found meal whit id = " + id);
+		return meal != null && meal.getUser().getId() == userId ? meal : null;
 	}
 
 	@Override
 	public List<Meal> getAll(int userId) {
-		return em.createQuery("SELECT m FROM Meal m WHERE m.user.id = userId ORDER BY m.dateTime DESC")
+		return em.createNamedQuery(Meal.ALL_SORTED, Meal.class)
+				.setParameter("userId", userId)
 				.getResultList();
 	}
 
 	@Override
 	public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-		List<Meal> meals = em.createQuery("SELECT m FROM Meal m WHERE m.user.id = " + userId + " ORDER BY m.dateTime DESC")
+		return em.createNamedQuery(Meal.GET_BETWEEN, Meal.class)
+				.setParameter("userId", userId)
+				.setParameter("startDateTime", startDateTime)
+				.setParameter("endDateTime", endDateTime)
 				.getResultList();
-
-		List<Meal> finalList = new ArrayList<>();
-		for (Meal m : meals) {
-			if (m.getDateTime().isAfter(startDateTime) &&
-					m.getDateTime().isBefore(endDateTime))
-				finalList.add(m);
-		}
-
-		return finalList;
 	}
 }
