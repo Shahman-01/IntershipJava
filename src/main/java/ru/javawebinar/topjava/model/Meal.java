42dfd5ce3bd6bacbdb239d58f2aa13cabb583be9
Shahman Magomadov <magomadovshahman@gmail.com>
2022-11-03 21:37:13 +0300

    cleaning

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -9,101 +9,101 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @NamedQueries({
-        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id =: userId ORDER BY m.dateTime DESC"),
-        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id =: userId"),
-        @NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m WHERE m.user.id =: userId " +
-                "AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC")
+		@NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id =: userId ORDER BY m.dateTime DESC"),
+		@NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id =: userId"),
+		@NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m WHERE m.user.id =: userId " +
+				"AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC")
 })
 @Entity
 @Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
 
-    public static final String ALL_SORTED = "Meal.getAll";
-    public static final String DELETE = "Meal.delete";
-    public static final String GET_BETWEEN = "Meal.getBetween";
+	public static final String ALL_SORTED = "Meal.getAll";
+	public static final String DELETE = "Meal.delete";
+	public static final String GET_BETWEEN = "Meal.getBetween";
 
-    @Column(name = "date_time", nullable = false, unique = true)
-    @NotNull
-    private LocalDateTime dateTime;
+	@Column(name = "date_time", nullable = false, unique = true)
+	@NotNull
+	private LocalDateTime dateTime;
 
-    @Column(name = "description", nullable = false)
-    @NotBlank
-    @Size(min = 2, max = 128)
-    private String description;
+	@Column(name = "description", nullable = false)
+	@NotBlank
+	@Size(min = 2, max = 128)
+	private String description;
 
 
-    @Column(name = "calories", nullable = false)
-    @NotNull
-    private int calories;
+	@Column(name = "calories", nullable = false)
+	@NotNull
+	private int calories;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    private User user;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private User user;
 
-    public Meal() {
-    }
+	public Meal() {
+	}
 
-    public Meal(Meal m) {
-        this(m.id, m.dateTime, m.description, m.calories);
-    }
+	public Meal(Meal m) {
+		this(m.id, m.dateTime, m.description, m.calories);
+	}
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
-    }
+	public Meal(LocalDateTime dateTime, String description, int calories) {
+		this(null, dateTime, description, calories);
+	}
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        super(id);
-        this.dateTime = dateTime;
-        this.description = description;
-        this.calories = calories;
-    }
+	public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+		super(id);
+		this.dateTime = dateTime;
+		this.description = description;
+		this.calories = calories;
+	}
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public int getCalories() {
-        return calories;
-    }
+	public int getCalories() {
+		return calories;
+	}
 
-    public LocalDate getDate() {
-        return dateTime.toLocalDate();
-    }
+	public LocalDate getDate() {
+		return dateTime.toLocalDate();
+	}
 
-    public LocalTime getTime() {
-        return dateTime.toLocalTime();
-    }
+	public LocalTime getTime() {
+		return dateTime.toLocalTime();
+	}
 
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setCalories(int calories) {
-        this.calories = calories;
-    }
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    @Override
-    public String toString() {
-        return "Meal{" +
-                "id=" + id +
-                ", dateTime=" + dateTime +
-                ", description='" + description + '\'' +
-                ", calories=" + calories +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Meal{" +
+				"id=" + id +
+				", dateTime=" + dateTime +
+				", description='" + description + '\'' +
+				", calories=" + calories +
+				'}';
+	}
 }
