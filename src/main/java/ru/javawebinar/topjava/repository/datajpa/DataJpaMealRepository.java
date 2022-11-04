e6ef133bf9b3710482ec5e2a3e18512935a2fb41
Shahman Magomadov <magomadovshahman@gmail.com>
2022-11-04 22:36:06 +0300

    did DataJpaMealRepo

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -6,6 +6,7 @@ import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
@@ -18,22 +19,29 @@ public class DataJpaMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        return meal.getUser().getId() == userId ? crudRepository.save(meal) : null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
+        Meal meal = crudRepository.getReferenceById(id);
+        if (meal.getUser().getId() == userId) {
+            crudRepository.delete(meal);
+            return true;
+        }
         return false;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal meal = crudRepository.getReferenceById(id);
+        return meal.getUser().getId() == userId ? meal : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findAll().stream()
+                .filter(m -> m.getUser().getId() == userId).collect(Collectors.toList());
     }
 
     @Override
