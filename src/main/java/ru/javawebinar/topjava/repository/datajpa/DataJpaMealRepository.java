d812491dfc5395255c3a5d88c3200db9c3a461aa
Shahman Magomadov <magomadovshahman@gmail.com>
2022-11-04 22:57:52 +0300

    clean

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -11,42 +11,42 @@ import java.util.stream.Collectors;
 @Repository
 public class DataJpaMealRepository implements MealRepository {
 
-    private final CrudMealRepository crudRepository;
+	private final CrudMealRepository crudRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
-        this.crudRepository = crudRepository;
-    }
+	public DataJpaMealRepository(CrudMealRepository crudRepository) {
+		this.crudRepository = crudRepository;
+	}
 
-    @Override
-    public Meal save(Meal meal, int userId) {
-        return meal.getUser().getId() == userId ? crudRepository.save(meal) : null;
-    }
+	@Override
+	public Meal save(Meal meal, int userId) {
+		return meal.getUser().getId() == userId ? crudRepository.save(meal) : null;
+	}
 
-    @Override
-    public boolean delete(int id, int userId) {
-        Meal meal = crudRepository.getReferenceById(id);
-        if (meal.getUser().getId() == userId) {
-            crudRepository.delete(meal);
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean delete(int id, int userId) {
+		Meal meal = crudRepository.getReferenceById(id);
+		if (meal.getUser().getId() == userId) {
+			crudRepository.delete(meal);
+			return true;
+		}
+		return false;
+	}
 
-    @Override
-    public Meal get(int id, int userId) {
-        Meal meal = crudRepository.getReferenceById(id);
-        return meal.getUser().getId() == userId ? meal : null;
-    }
+	@Override
+	public Meal get(int id, int userId) {
+		Meal meal = crudRepository.getReferenceById(id);
+		return meal.getUser().getId() == userId ? meal : null;
+	}
 
-    @Override
-    public List<Meal> getAll(int userId) {
-        return crudRepository.findAll().stream()
-                .filter(m -> m.getUser().getId() == userId).collect(Collectors.toList());
-    }
+	@Override
+	public List<Meal> getAll(int userId) {
+		return crudRepository.findAll().stream()
+				.filter(m -> m.getUser().getId() == userId).collect(Collectors.toList());
+	}
 
-    @Override
-    public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return crudRepository.findAll().stream()
-                .filter(m -> m.getUser().getId() == userId).collect(Collectors.toList());
-    }
+	@Override
+	public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+		return crudRepository.findAll().stream()
+				.filter(m -> m.getUser().getId() == userId).collect(Collectors.toList());
+	}
 }
