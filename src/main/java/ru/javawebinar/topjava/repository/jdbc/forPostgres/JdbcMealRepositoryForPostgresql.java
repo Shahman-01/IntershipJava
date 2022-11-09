6d4ecfbe8f7579044887810daad385f4f965a491
Shahman Magomadov <magomadovshahman@gmail.com>
2022-11-09 22:50:40 +0300

    doing JdbcMealForPostgres

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/forPostgres/JdbcMealRepositoryForPostgresql.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/forPostgres/JdbcMealRepositoryForPostgresql.java
@@ -1,4 +1,55 @@
 package ru.javawebinar.topjava.repository.jdbc.forPostgres;
 
-public class JdbcMealRepositoryForPostgresql {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.jdbc.AbstractJdbcMealRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class JdbcMealRepositoryForPostgresql extends AbstractJdbcMealRepository {
+	public JdbcMealRepositoryForPostgresql(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate, JdbcTemplate jdbcTemplate1, NamedParameterJdbcTemplate namedParameterJdbcTemplate1, SimpleJdbcInsert insertMeal) {
+		super(jdbcTemplate, namedParameterJdbcTemplate, jdbcTemplate1, namedParameterJdbcTemplate1, insertMeal);
+	}
+
+	@Override
+	public Meal save(Meal meal, int userId) {
+		MapSqlParameterSource map = new MapSqlParameterSource()
+				.addValue("id", meal.getId())
+				.addValue("description", meal.getDescription())
+				.addValue("calories", meal.getCalories())
+				.addValue("date_time", meal.getDateTime())
+				.addValue("user_id", userId);
+
+		if (meal.isNew()) {
+			Number newId = insertMeal.executeAndReturnKey(map);
+			meal.setId(newId.intValue());
+		} else {
+			if (namedParameterJdbcTemplate.update("" +
+					"UPDATE meals " +
+					"   SET description=:description, calories=:calories, date_time=:date_time " +
+					" WHERE id=:id AND user_id=:user_id", map) == 0) {
+				return null;
+			}
+		}
+		return meal;
+	}
+
+
+	@Override
+	public List<Meal> getAll(int userId) {
+		return jdbcTemplate.query(
+				"SELECT * FROM meals WHERE user_id=? ORDER BY date_time DESC", ROW_MAPPER, userId);
+	}
+
+	@Override
+	public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+		return jdbcTemplate.query(
+				"SELECT * FROM meals WHERE user_id=?  AND date_time >=  ? AND date_time < ? ORDER BY date_time DESC",
+				ROW_MAPPER, userId, startDateTime, endDateTime);
+	}
 }
