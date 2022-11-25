bd4d9023313e4eef65958720daa41e54efa64c90
Shahman Magomadov <magomadovshahman@gmail.com>
2022-11-25 19:34:47 +0300

    did AdminRestController

--- src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -1,39 +1,58 @@
 package ru.javawebinar.topjava.web.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.User;
 
+import java.net.URI;
 import java.util.List;
 
 @Controller
+@RequestMapping(value = AdminRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdminRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/admin/users";
 
     @Override
+    @GetMapping
     public List<User> getAll() {
         return super.getAll();
     }
 
     @Override
+    @GetMapping("/{id}")
     public User get(int id) {
         return super.get(id);
     }
 
-    @Override
-    public User create(User user) {
-        return super.create(user);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@RequestBody User user) {
+        User created = super.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
     @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(int id) {
         super.delete(id);
     }
 
     @Override
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(User user, int id) {
         super.update(user, id);
     }
 
     @Override
+    @GetMapping("/by-email")
     public User getByMail(String email) {
         return super.getByMail(email);
     }
