2fc810cceac9ce8c5e24125f971d6b928a258397
Shahman Magomadov <magomadovshahman@gmail.com>
2022-11-28 17:42:05 +0300

    clearing

--- src/main/java/ru/javawebinar/topjava/repository/JpaUtil.java
+++ src/main/java/ru/javawebinar/topjava/repository/JpaUtil.java
@@ -14,8 +14,6 @@ public class JpaUtil {
     public void clear2ndLevelHibernateCache() {
         Session s = (Session) em.getDelegate();
         SessionFactory sf = s.getSessionFactory();
-//        sf.getCache().evictEntityData(User.class, AbstractBaseEntity.START_SEQ);
-//        sf.getCache().evictEntityData(User.class);
         sf.getCache().evictAllRegions();
     }
 }
